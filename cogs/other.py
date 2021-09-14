fb69a2702d72edbb5582177ab38456c41da2d907
1THGAMER1 <65722663+1THGAMER1@users.noreply.github.com>
2021-09-14 16:41:26 +0200

    Update other.py

--- cogs/other.py
+++ cogs/other.py
@@ -33,6 +33,8 @@ class others(commands.Cog):
             description='Der Bot befindet sich in Version **1.1.0**\n \n**__NEU:__**\n-Neuer überarbeiteter Welcomer, nun kann jeder ganz einfach den Welcomer in seinen Server benutzen.\n-Submit Command in der Beta Version eingeführt. (Schreibe mich für neue Vorschläge oder Feedback an.\n-Oberflächenupdates der Antworten.\n-Weitere leine Fehlerbehebungen',
             colour = discord.Colour.blue()
             
+    ) 
+            
         version.set_footer(text='Ocean Bot Version 1.1.0',
         icon_url='https://cdn.discordapp.com/attachments/778307192465260588/809793314614149140/android-icon-96x96.png')
 
